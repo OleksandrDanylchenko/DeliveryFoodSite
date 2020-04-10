2c5cbd6e2427f1f537f9a4ccb10fefc50e567191
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-04-10 17:06:13 +0300

    Added JS to close and open modal window

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector("#cart-button").addEventListener("click", toggleModal);
+document.querySelectorAll(".modal-close, .button-close").forEach((item) => {
+  item.addEventListener("click", toggleModal);
+});
+
+let basketModal = document.querySelector(".modal");
+function toggleModal() {
+  basketModal.classList.toggle("is-open");
+}
